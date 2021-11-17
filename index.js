a22e3ef1aa7076df564711b31cea495ee56fed56
BobTheBuilder <podh@protonmail.com>
2021-11-17 10:45:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = 80;
  
 const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
