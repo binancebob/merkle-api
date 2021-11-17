b1ea81371281d3fdb9ce9c4d5588146db4c95d0b
BobTheBuilder <podh@protonmail.com>
2021-11-17 17:29:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
  
 const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
@@ -19,10 +27,3 @@ app.listen(port, () => {
   console.log(`Listening - ${port}`);
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
