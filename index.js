66dce4211fe48da930791df71e56c979c788addb
BobTheBuilder <podh@protonmail.com>
2021-11-17 17:24:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,11 @@ app.get('/proof/:address', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening - ${port}`);
 });
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
